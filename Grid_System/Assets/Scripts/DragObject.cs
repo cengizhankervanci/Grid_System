a074b08be947b79b477af8f5eaad4ebf33c4d4d6
cengizhankervanci <71005996+cengizhankervanci@users.noreply.github.com>
2020-10-10 14:13:44 +0300

    Grid System
    Grid system with mouse and 3D.

---
+++ Grid_System/Assets/Scripts/DragObject.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragObject : MonoBehaviour
+{
+    private float mZCoord;
+    void OnMouseDown()
+    {
+        mZCoord = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
+    }
+    private Vector3 GetMouseAsWorldPoint()
+    {
+        Vector3 mousePoint = Input.mousePosition;
+        mousePoint.z = mZCoord;
+        return Camera.main.ScreenToWorldPoint(mousePoint);
+    }
+
+    void OnMouseDrag()
+    {
+        transform.position = new Vector3(GetMouseAsWorldPoint().x, transform.position.y, GetMouseAsWorldPoint().z);
+    }
+}
---
+++ Grid_System/Assets/Scripts/MoveController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveController : MonoBehaviour
+{
+    public GameObject Player;
+    public GameObject target;
+    Vector3 truePos;
+    void LateUpdate()
+    {
+        truePos.x = Mathf.Floor(target.transform.position.x);
+        truePos.y = 0.5f;
+        truePos.z = Mathf.Floor(target.transform.position.z);
+        Player.transform.position = truePos;
+    }
+}
